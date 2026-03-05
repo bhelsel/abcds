2db942227cec7973ed7f0230b481dafc1960a923
Brian Helsel <bhelsel@kumc.edu>
2026-03-05 09:24:57 -0600

    Update calculate_kbit2_score.R

--- R/calculate_kbit2_score.R
+++ R/calculate_kbit2_score.R
@@ -26,19 +26,39 @@
 #' @importFrom foreach `%dopar%` foreach
 #' @importFrom plyr rbind.fill
 
-calculate_kbit2_score <- function(age_at_visit, kbit2verbkraw, kbit2ridraw, kbit2vnonvraw,
-                                  subject_label = NULL, add_premorbid_id = FALSE,
-                                  print_kbit = TRUE, doParallel = TRUE){
+calculate_kbit2_score <- function(
+  age_at_visit,
+  kbit2verbkraw,
+  kbit2ridraw,
+  kbit2vnonvraw,
+  subject_label = NULL,
+  add_premorbid_id = FALSE,
+  print_kbit = TRUE,
+  doParallel = TRUE
+) {
+  files <- list.files(
+    path = system.file("extdata", package = "abcds"),
+    pattern = "kbit2",
+    full.names = TRUE
+  )
 
-  files <- list.files(path = system.file("extdata", package = "abcds"), pattern = "kbit2", full.names = TRUE)
-
-  for(f in files) load(f)
-
-  if(length(kbit2verbkraw) > 1 & is.null(subject_label)) {
-    stop("A subject_label vector is required when calculating multiple KBIT-2 scores.")
+  for (f in files) {
+    load(f)
   }
 
-  main_kbit2_calculator <- function(age_at_visit, kbit2verbkraw, kbit2ridraw, kbit2vnonvraw, subject_label){
+  if (length(kbit2verbkraw) > 1 & is.null(subject_label)) {
+    stop(
+      "A subject_label vector is required when calculating multiple KBIT-2 scores."
+    )
+  }
+
+  main_kbit2_calculator <- function(
+    age_at_visit,
+    kbit2verbkraw,
+    kbit2ridraw,
+    kbit2vnonvraw,
+    subject_label
+  ) {
     # Calculate sum of standard scores
     kbit2verbraw <- kbit2verbkraw + kbit2ridraw
 
@@ -47,7 +67,8 @@ calculate_kbit2_score <- function(age_at_visit, kbit2verbkraw, kbit2ridraw, kbit
       dplyr::filter(
         min_age <= age_at_visit &
           max_age >= age_at_visit &
-          raw_score == kbit2verbraw) %>%
+          raw_score == kbit2verbraw
+      ) %>%
       dplyr::select(-c(min_age, max_age))
 
     kbit2verbstd <- verbstd$standard_score
@@ -59,7 +80,8 @@ calculate_kbit2_score <- function(age_at_visit, kbit2verbkraw, kbit2ridraw, kbit
       dplyr::filter(
         min_age <= age_at_visit &
           max_age >= age_at_visit &
-          raw_score == kbit2vnonvraw) %>%
+          raw_score == kbit2vnonvraw
+      ) %>%
       dplyr::select(-c(min_age, max_age))
 
     kbit2nonvbstd <- nonvbstd$standard_score
@@ -101,59 +123,97 @@ calculate_kbit2_score <- function(age_at_visit, kbit2verbkraw, kbit2ridraw, kbit
 
     data <-
       data.frame(
-        age_at_visit, kbit2verbkraw, kbit2ridraw, kbit2vnonvraw,
-        kbit2verbraw, kbit2verbstd, kbit2verbstdci, kbit2verbstdpr,
-        kbit2nonvbstd, kbit2nonvbstdci, kbit2nonvbstdpr, kbit2iqcompstd,
-        kbit2iqcompci, kbit2iqcomppr, kbit2nonvbaey, kbit2nonvbae,
-        kbit2verbaey, kbit2verbae
+        age_at_visit,
+        kbit2verbkraw,
+        kbit2ridraw,
+        kbit2vnonvraw,
+        kbit2verbraw,
+        kbit2verbstd,
+        kbit2verbstdci,
+        kbit2verbstdpr,
+        kbit2nonvbstd,
+        kbit2nonvbstdci,
+        kbit2nonvbstdpr,
+        kbit2iqcompstd,
+        kbit2iqcompci,
+        kbit2iqcomppr,
+        kbit2nonvbaey,
+        kbit2nonvbae,
+        kbit2verbaey,
+        kbit2verbae
       )
 
-    if(!is.null(subject_label)) data <- cbind(subject_label, data)
+    if (!is.null(subject_label)) {
+      data <- cbind(subject_label, data)
+    }
 
-    if(add_premorbid_id){
-      if(data$kbit2iqcompstd > 40){
+    if (add_premorbid_id) {
+      if (data$kbit2iqcompstd > 40) {
         data$prefunclevel <- ifelse(data$kbit2iqcompstd >= 50, 1, 2)
-      } else if (data$kbit2iqcompstd == 40){
+      } else if (data$kbit2iqcompstd == 40) {
         data$prefunclevel <-
-          ifelse(aenonvb$age_equivalent[1] %in% c("< 4:0", "4:0") &
-                   aeverb$age_equivalent[1] %in% c("< 4:0", "4:0"), 3, 2)
+          ifelse(
+            aenonvb$age_equivalent[1] %in%
+              c("< 4:0", "4:0") &
+              aeverb$age_equivalent[1] %in% c("< 4:0", "4:0"),
+            3,
+            2
+          )
       }
     }
     return(data)
   }
 
-  if(doParallel & length(kbit2verbkraw) > 1){
+  if (doParallel & length(kbit2verbkraw) > 1) {
     cores = parallel::detectCores()
     Ncores = cores - 1
     cl = parallel::makeCluster(Ncores)
     doParallel::registerDoParallel(cl)
     `%dopar%` = foreach::`%dopar%`
     kbitres <-
-      foreach::foreach(i = 1:length(kbit2verbkraw), .packages = "abcds") %dopar% {
-        main_kbit2_calculator(age_at_visit[i], kbit2verbkraw[i], kbit2ridraw[i], kbit2vnonvraw[i], subject_label[i])
-    } %>%
+      foreach::foreach(i = 1:length(kbit2verbkraw), .packages = "abcds") %dopar%
+      {
+        main_kbit2_calculator(
+          age_at_visit[i],
+          kbit2verbkraw[i],
+          kbit2ridraw[i],
+          kbit2vnonvraw[i],
+          subject_label[i]
+        )
+      } %>%
       dplyr::bind_rows() %>%
       dplyr::arrange(subject_label)
-  } else if (!doParallel & length(kbit2verbkraw) > 1){
+  } else if (!doParallel & length(kbit2verbkraw) > 1) {
     kbitres <- data.frame()
-    for(i in 1:length(kbit2verbkraw)){
+    for (i in 1:length(kbit2verbkraw)) {
       cat("\r Processing data for ", subject_label[i])
-      data <- main_kbit2_calculator(age_at_visit[i], kbit2verbkraw[i], kbit2ridraw[i], kbit2vnonvraw[i], subject_label[i])
+      data <- main_kbit2_calculator(
+        age_at_visit[i],
+        kbit2verbkraw[i],
+        kbit2ridraw[i],
+        kbit2vnonvraw[i],
+        subject_label[i]
+      )
       kbitres <- plyr::rbind.fill(kbitres, data)
     }
     kbitres <- dplyr::arrange(kbitres, subject_label)
-  } else if(length(kbit2verbkraw) == 1){
-    kbitres <- main_kbit2_calculator(age_at_visit, kbit2verbkraw, kbit2ridraw, kbit2vnonvraw, subject_label)
+  } else if (length(kbit2verbkraw) == 1) {
+    kbitres <- main_kbit2_calculator(
+      age_at_visit,
+      kbit2verbkraw,
+      kbit2ridraw,
+      kbit2vnonvraw,
+      subject_label
+    )
     kbitres$subject_label <- NULL
-    if(print_kbit) print_kbit2_results(kbitres)
-  } else{
+    if (print_kbit) print_kbit2_results(kbitres)
+  } else {
     stop("An error occurred. Please check your data and try again.")
   }
 
   class(kbitres) <- c("tbl_df", "tbl", "data.frame")
 
   return(kbitres)
-
 }
 
 #' @title print_kbit2_results
@@ -164,33 +224,42 @@ calculate_kbit2_score <- function(age_at_visit, kbit2verbkraw, kbit2ridraw, kbit
 #' @rdname print_kbit2_results
 #' @export
 
-print_kbit2_results <- function(kbitres){
-
+print_kbit2_results <- function(kbitres) {
   message(
     sprintf(
       "KBIT-2 results for a %i-year old participant with a verbal score of %i and nonverbal score of %i.",
-      kbitres$age_at_visit, kbitres$kbit2verbraw, kbitres$kbit2vnonvraw
-      ), "\n\n",
+      kbitres$age_at_visit,
+      kbitres$kbit2verbraw,
+      kbitres$kbit2vnonvraw
+    ),
+    "\n\n",
     sprintf(
       "Verbal Standard Score: %i (90%% CI: %s); Percentile Rank: %s",
-      kbitres$kbit2verbstd, kbitres$kbit2verbstdci, kbitres$kbit2verbstdpr
-      ), "\n",
+      kbitres$kbit2verbstd,
+      kbitres$kbit2verbstdci,
+      kbitres$kbit2verbstdpr
+    ),
+    "\n",
     sprintf(
       "Nonverbal Standard Score: %i (90%% CI: %s); Percentile Rank: %s",
-      kbitres$kbit2nonvbstd, kbitres$kbit2nonvbstdci, kbitres$kbit2nonvbstdpr
-    ), "\n",
+      kbitres$kbit2nonvbstd,
+      kbitres$kbit2nonvbstdci,
+      kbitres$kbit2nonvbstdpr
+    ),
+    "\n",
     sprintf(
       "Intelligent Quotient %i (90%% CI: %s); Percentile Rank: %s",
-      kbitres$kbit2iqcompstd, kbitres$kbit2iqcompci, kbitres$kbit2iqcomppr
-    ), "\n\n",
+      kbitres$kbit2iqcompstd,
+      kbitres$kbit2iqcompci,
+      kbitres$kbit2iqcomppr
+    ),
+    "\n\n",
     sprintf(
       "Verbal Age Equivalent: %s:%s; Nonverbal Age Equivalent: %s:%s",
-      kbitres$kbit2verbaey, kbitres$kbit2verbae,
-      kbitres$kbit2nonvbaey, kbitres$kbit2nonvbae
+      kbitres$kbit2verbaey,
+      kbitres$kbit2verbae,
+      kbitres$kbit2nonvbaey,
+      kbitres$kbit2nonvbae
     )
   )
-
-
 }
-
-
